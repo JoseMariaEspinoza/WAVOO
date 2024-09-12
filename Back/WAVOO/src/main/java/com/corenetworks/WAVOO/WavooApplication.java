3f054ce7e45732d9f3ddeaf96974ead6e959f1bc
JoseMariaEspinoza <josemaria.espinoza.adm@gmail.com>
2024-09-12 11:44:38 +0200

    prueba inicio

---
+++ Back/WAVOO/src/main/java/com/corenetworks/WAVOO/WavooApplication.java
@@ -0,0 +1,13 @@
+package com.corenetworks.WAVOO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WavooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WavooApplication.class, args);
+	}
+
+}
---
+++ Back/WAVOO/src/test/java/com/corenetworks/WAVOO/WavooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corenetworks.WAVOO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WavooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
